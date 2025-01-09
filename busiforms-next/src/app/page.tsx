4b6b25407fd31d62fd5b36a22f1fc5412145ad4a
Paul Park <2000mageia@gmail.com>
2025-01-09 16:45:47 +0900

    change mainpage to button

--- busiforms-next/src/app/page.tsx
+++ busiforms-next/src/app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from 'next/navigation';
+"use client";
+import Button from "@/components/common/Button/Button";
+import { Container } from "@chakra-ui/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect('/login');
+  return (
+    <Container>
+      <h1>Welcome to BusiForms</h1>
+      <Button onClick={() => redirect("/auth/login")}>Login</Button>
+    </Container>
+  );
 }
