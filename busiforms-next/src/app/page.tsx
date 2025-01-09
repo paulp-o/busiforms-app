a6575340c5fbdc613c3b20455553ac301e9531a6
Paul Park <2000mageia@gmail.com>
2025-01-09 16:51:38 +0900

    replace button redirect with link for login navigation

--- busiforms-next/src/app/page.tsx
+++ busiforms-next/src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Button from "@/components/common/Button/Button";
 import { Container } from "@chakra-ui/react";
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Container>
       <h1>Welcome to BusiForms</h1>
-      <Button onClick={() => redirect("/auth/login")}>Login</Button>
+      <Link href="/auth/login">
+        <Button>Login</Button>
+      </Link>
     </Container>
   );
 }
