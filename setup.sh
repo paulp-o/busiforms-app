4bb8c71fc242ecdb1016b833d9a5d1ef139d58e3
Paul Park <2000mageia@gmail.com>
2025-01-09 16:30:02 +0900

    refactor setup script to streamline Prisma setup process

--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ cd busiforms-nest
 echo "Installing npm dependencies in busiforms-nest..."
 npm install
 
+echo "=== Running Prisma setup in busiforms-nest ==="
+npx prisma migrate dev --name init # Initializes Prisma Client
+npx prisma generate # Generates Prisma Client
+
 echo "=== Setting up Next.js Project ==="
 cd ../busiforms-next
 echo "Installing npm dependencies in busiforms-next..."
 npm install
-
-echo "=== Running Prisma setup ==="
-
-npx prisma migrate dev --name init # Initializes Prisma Client
-npx prisma generate # Generates Prisma Client
